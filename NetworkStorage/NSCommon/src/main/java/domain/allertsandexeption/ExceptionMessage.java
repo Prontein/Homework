00cc1d4a55a53a49b68c4d05761df0911feb8574
Prontein <77588897+Prontein@users.noreply.github.com>
2021-07-08 20:19:14 +0500

    Delete NetworkStorage/NSCommon directory

--- NetworkStorage/NSCommon/src/main/java/domain/allertsandexeption/ExceptionMessage.java
+++
@@ -1,6 +0,0 @@
-package domain.allertsandexeption;
-
-public class ExceptionMessage {
-    public static final String PROPFILE_ERROR = "Неверно задан Properties файл";
-    public static final String READ_FILE_ERROR = "Ошибка при чтении файла";
-}
